83f03bf06f3259356066848ae61ba1835154a591
mahmud2237 <smahmud2237@gmail.com>
2024-12-09 21:29:04 +0600

    Blog button updated

--- js/second.js
+++ js/second.js
@@ -2,11 +2,20 @@ document.getElementById('btn-history').addEventListener('click', function(){
     // show history section
     document.getElementById('history-section').classList.remove('hidden');
     document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('blog-section').classList.add('hidden');
     
 })
 document.getElementById('btn-donation').addEventListener('click', function(){
-    // show history section
+    // show donation section
     document.getElementById('donation-section').classList.remove('hidden');
     document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('blog-section').classList.add('hidden');
+    
+})
+document.getElementById('btn-blog').addEventListener('click', function(){
+    // show blog section
+    document.getElementById('blog-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('donation-section').classList.add('hidden');
     
 })
