e14479adf29686b0cbeadb31a841b2a894dc75e7
mahmud2237 <smahmud2237@gmail.com>
2024-12-09 21:04:35 +0600

    Button event Listener added

--- js/first.js
+++ js/first.js
@@ -1,27 +1,45 @@
 
 
 let totalBalance = document.getElementById('total-balance');
-let donation1 = document.getElementById('donated1');
-let donation2 = document.getElementById('donated2');
-let donation3= document.getElementById('donated3');
+
+let donated1 = document.getElementById('donated1');
+
+
+let donated2 = document.getElementById('donated2');
+// console.log(donation2);
+let donated3 = document.getElementById('donated3');
+// console.log(donation3);
+
 
 let donateBtn1 = document.getElementById('button1');
 let donateInput1 = document.getElementById('donate-input1');
+
 let donateBtn2 = document.getElementById('button2');
 let donateInput2 = document.getElementById('donate-input2');
+
 let donateBtn3 = document.getElementById('button3');
 let donateInput3 = document.getElementById('donate-input3');
 
 
-donateInput1.addEventListener('keyup', function () {
+donateInput1.addEventListener('keyup', function (event) {
+    event.preventDefault();
     const donation = parseFloat(donateInput1.value);
     if (typeof donation === 'number' && donation > 0) {
         donateBtn1.disabled = false;
+        donateBtn1.addEventListener('click', function () {
+            const totalBalanceNum = parseFloat(totalBalance.innerText);
+            const newBalance = totalBalanceNum - donation;
+            totalBalance.innerText = newBalance;
+            const donated1Num = parseFloat(donated1.innerText);
+            const plusBalance = donated1Num + donation;
+            donated1.innerText = plusBalance;
+        })
     } else {
         donateBtn1.disabled = true;
     }
 })
-donateInput2.addEventListener('keyup', function () {
+
+donateInput2.addEventListener('keydown', function () {
     const donation = parseFloat(donateInput2.value);
     if (typeof donation === 'number' && donation > 0) {
         donateBtn2.disabled = false;
--- js/second.js
+++ js/second.js
@@ -1,1 +1,12 @@
-console.log('second.js connected');
+document.getElementById('btn-history').addEventListener('click', function(){
+    // show history section
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('donation-section').classList.add('hidden');
+    
+})
+document.getElementById('btn-donation').addEventListener('click', function(){
+    // show history section
+    document.getElementById('donation-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    
+})
